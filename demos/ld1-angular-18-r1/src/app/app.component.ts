2a75c3f0b21a05cc81e450f772d4a1512c87cc59
techie-dev <techie-dev@techie-dev.com>
2025-02-20 14:50:38 +0530

    commit!

--- demos/ld1-angular-18-r1/src/app/app.component.ts
+++ demos/ld1-angular-18-r1/src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// The import keyword is used to import functions exported by an external module.
+// The OnInit interface is used to define a lifecycle hook for the Angular component.
+// The Component decorator is used to define the metadata for the Angular component.
+// The Component decorator is a function that takes a metadata object as an argument.
+// The metadata object contains the configuration information for the component.
+
 import { RouterOutlet } from '@angular/router';
+import { EmployeeService } from './service/employee.service';
+import { Employee } from './model/employee.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'The ld1-angular-18-r1 App';
+
+// The employees property is an array of Employee objects.
+  employees? : Employee[];
+  constructor(private employeeService : EmployeeService){
+
+  }
+  ngOnInit(): void {
+    console.log("asynchronously fetching data from endpoint!")
+    this.employeeService.getEmployees().subscribe(datafromExeternalService => this.employees = datafromExeternalService);
+    console.log(this.employees)
+  }
 }
---
+++ demos/ld1-angular-18-r1/src/app/service/employee.service.ts
@@ -0,0 +1,25 @@
+
+import { HttpClient } from "@angular/common/http";
+import {Employee } from "../model/employee.model";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class EmployeeService {
+    baseUrl : string = 'http://localhost:3000/employees';
+    // httpClient? : HttpClient;
+    // constructor(httpClient : HttpClient){
+    //     this.httpClient = httpClient;
+    // }
+    constructor(private httpClient : HttpClient){
+
+    }
+    getEmployees(){
+        console.log("Iniside employees service!")
+        return this.httpClient.get<Employee[]>(this.baseUrl);
+    }
+    createEmployee(employee : Employee){
+        return this.httpClient.post(this.baseUrl,employee);
+    }
+}
