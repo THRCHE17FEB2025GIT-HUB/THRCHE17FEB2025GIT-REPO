854be0ed9e5276e005f873df96ec4109c90accb5
techie-dev <techie-dev@techie-dev.com>
2025-02-25 14:37:40 +0530

    commit!

---
+++ demos/lp1b-EFicitiShopAngularSPA-NG18/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { ListEmpComponent } from "./list-emp/list-emp.component";
+import { UpdateEmpComponent } from "./update-emp/update-emp.component";
+import { RegisterComponent } from "./register/register.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { EmployeeService } from "./service/employee.service";
+
+@NgModule({
+    imports: [BrowserModule,
+        // HttpClientModule,
+        // ReactiveFormsModule,
+        // FormsModule,
+        AppRoutingModule,
+    ],
+    declarations: [AppComponent, LoginComponent, ListEmpComponent, UpdateEmpComponent, RegisterComponent],
+    bootstrap: [AppComponent],
+    providers: [EmployeeService]
+})
+export class AppModule {
+
+}
---
+++ demos/lp1b-EFicitiShopAngularSPA-NG18/src/app/update-emp/update-emp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-emp',
+  //  standalone: true,
+  // imports: [],
+  templateUrl: './update-emp.component.html',
+  styleUrl: './update-emp.component.css'
+})
+export class UpdateEmpComponent {
+
+}
